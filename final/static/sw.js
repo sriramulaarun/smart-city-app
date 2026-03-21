181d2f69e5fd94218a23a36b7413ad4a840e8edc
Sriramula arun <104306712+sriramulaarun@users.noreply.github.com>
2026-03-21 12:17:12 +0530

    Add files via upload

---
+++ final/static/sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'smartcity-v1';
+const ASSETS = [
+  '/',
+  '/static/css/style.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
